9f1b728958241cad12b99dcd1479fe1e9420830e
Cristofer Gaona <95365103+crirgl@users.noreply.github.com>
2023-03-10 05:44:29 +0000

    ultima-prueba 3

---
+++ mass-lib.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+/*nor permite un tipo de variablesar*/
+
+/*En C++ no hay tipo string, por ello, hay que importarlo de una libreria.
+
+iostream: Entrada y salida de datos.
+string: Manejo de cadenas de texto.
+cmath: Funciones matematicas comunes.
+exception: Utilidades para el manejo de errores.
+array: Manejo de arreglos de datos.
+vector: Manejo de vectores de datos.*/
